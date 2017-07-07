f7c6415c83083fbfc2cbe2caececd8ced1a92830
Aidan Timson <atimson123@gmail.com>
2017-07-07 09:46:10 +0100

    Initial Commit

---
+++ List.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace DynamicList
+{
+    partial class DynamicList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // listView
+            // 
+            this.listView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView.FullRowSelect = true;
+            this.listView.Location = new System.Drawing.Point(0, 0);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(1014, 677);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // DynamicList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 677);
+            this.Controls.Add(this.listView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "DynamicList";
+            this.Text = "Dynamic List";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView;
+    }
+}
+
---
+++ List.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DynamicList
+{
+    public partial class DynamicList : Form
+    {
+        public DynamicList()
+        {
+            InitializeComponent();
+        }
+
+        public void AddColumn(string headingText, int width = 1, HorizontalAlignment horizontalAlignment = HorizontalAlignment.Left)
+        {
+            listView.Columns.Add(headingText, width, horizontalAlignment);
+        }
+
+        public void AddEntry(string text)
+        {
+            listView.Items.Add(text);
+        }
+
+        public void SetCheckboxes(bool on)
+        {
+            listView.CheckBoxes = on;
+        }
+
+        public void AutoResizeColumns()
+        {
+            listView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+        }
+
+        public void SetBackgroundColor(string hexColor)
+        {
+            listView.BackColor = ColorTranslator.FromHtml(hexColor);
+        }
+
+        public void SetForegroundColor(string hexColor)
+        {
+            listView.ForeColor = ColorTranslator.FromHtml(hexColor);
+        }
+
+        public void SetFont(string familyName = "Microsoft Sans Serif", float size = 8.25f, FontStyle style = FontStyle.Regular)
+        {
+            listView.Font = new Font(new FontFamily(familyName), size, style);
+        }
+
+        public void BorderStyle()
+        {
+
+        }
+
+    }
+}
